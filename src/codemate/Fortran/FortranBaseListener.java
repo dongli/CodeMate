6db5582d3d2564576acce982ff106d19ba24e149
Li Dong <dongli@lasg.iap.ac.cn>
2013-07-31 12:09:09 +0800

    Add license stuffs

--- src/codemate/Fortran/FortranBaseListener.java
+++ src/codemate/Fortran/FortranBaseListener.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Li Dong.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Li Dong - initial API and implementation
+ ******************************************************************************/
 // Generated from Fortran.g4 by ANTLR 4.1
 
     package codemate.Fortran;
--- src/codemate/librarymate/MPILibraryMate.java
+++ src/codemate/librarymate/MPILibraryMate.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Li Dong.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Li Dong - initial API and implementation
+ ******************************************************************************/
 package codemate.librarymate;
 
 import java.util.*;
